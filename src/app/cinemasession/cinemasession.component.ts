4e8df94fe91e4ac702a3b7ac93ccd2ca98d16beb
Mustafalou <yilmaz121030@hotmail.be>
2023-01-22 00:28:44 +0100

    websocket working with my api

--- src/app/cinemasession/cinemasession.component.ts
+++ src/app/cinemasession/cinemasession.component.ts
@@ -1,12 +1,29 @@
-import { Component, ViewChild , ElementRef } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { WebsocketService } from 'src/services/web-socket.service';
+
 @Component({
   selector: 'app-cinemasession',
   templateUrl: './cinemasession.component.html',
-  styleUrls: ['./cinemasession.component.sass']
+  styleUrls: ['./cinemasession.component.sass'],
+  providers: [WebsocketService]
 })
 export class CinemasessionComponent {
+  private socket: WebSocket;
+  constructor(){
+    this.socket = new WebSocket('wss://mustafalou.com/ws');
+    this.socket.onopen = (event) => {
+      console.log('WebSocket connection established', event);
+    };
+  }
+  startListener(){
+    const iframe = document.getElementById('video') as HTMLIFrameElement;
+    iframe.contentWindow?.document.querySelector('button[data-button-listener="play"]')?.addEventListener('click', () => {
+      this.socket.send('play');
+    })
+  }
+  public sendMessage() {
+    this.socket.send('Hello server');
+  }
+
 }
 
